52e1063e3a72c96fa863c5bae6f98398b9a41c82
John Stableford <john.stableford@gmail.com>
2021-09-09 13:40:42 +1000

    chore: stableford-completed

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,4 +1,25 @@
 const resolvers = {
+  Mutation: {
+    incrementTrackViews: async (_, { id }, { dataSources }) => {
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(id);
+        
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track,
+        };
+      } catch (err) {
+        return {
+          code: err.extensions.response.status,
+          success: false,
+          message: err.extensions.response.body,
+          track: null,
+        }
+      }
+    }
+  },
   Query: {
     // returns an array of Tracks that will be used to populate the homepage grid of our web client
     tracksForHome: (_, __, { dataSources }) => {
